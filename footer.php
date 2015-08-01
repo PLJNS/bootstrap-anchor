d4c9f2853f0b85cc6cac63d17c2dde063a5885b2
root <root@45.79.151.160>
2015-08-01 01:35:17 +0000

    added footer

--- footer.php
+++ footer.php
@@ -1,22 +1,29 @@
-		<div class="wrap">
-            <footer id="bottom">
-                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
 
-                <ul role="navigation">
-                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
-                    <?php if(twitter_account()): ?>
-                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
-                    <?php endif; ?>
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-5 text-left hidden-xs">
+				<p id="footer-copyright">&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</p>
+			</div>
+			<div class="col-xs-7 text-right hidden-xs">
+				<ul class="list-inline">
+			<li><a href="<?php echo rss_url(); ?>">RSS</a></li>
+			<?php if(twitter_account()): ?>
+			<li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
+			<?php endif; ?>
+			<li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
+			<li><a href="/" title="Return to my website.">Home</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-12 text-center visible-xs">
+				<p id="footer-copyright">© 2015 PLJNS, LLC.</p>
+			</div>
+		</div>
+	</div>
+</footer>
 
-                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
-
-                    <li><a href="/" title="Return to my website.">Home</a></li>
-                </ul>
-            </footer>
-            
-        </div>
-        <script src="<?php echo theme_url('/js/main.js'); ?>"></script>
-        <script src="<?php echo theme_url('/js/jquery.min.js'); ?>"></script>
-        <script src="<?php echo theme_url('/js/bootstrap.min.js'); ?>"></script>
-    </body>
+		<script src="<?php echo theme_url('/js/main.js'); ?>"></script>
+		<script src="<?php echo theme_url('/js/jquery.min.js'); ?>"></script>
+		<script src="<?php echo theme_url('/js/bootstrap.min.js'); ?>"></script>
+	</body>
 </html>
--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 		<meta name="description" content="<?php echo site_description(); ?>">
 
 		<link rel="stylesheet" href="<?php echo theme_url('/css/bootstrap.min.css'); ?>">
+		<link rel="stylesheet" href="<?php echo theme_url('/css/style.css'); ?>">
 
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo rss_url(); ?>">
 		<link rel="shortcut icon" href="<?php echo theme_url('img/favicon.png'); ?>">
