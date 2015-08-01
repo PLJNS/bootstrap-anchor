ebc4e48c3d03bd5b8b29edc8a535cbb51951230d
root <root@45.79.151.160>
2015-08-01 00:59:06 +0000

    added jquery, gitignore, initial bootstrap header

--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
 		<div class="wrap">
-	            <footer id="bottom">
-	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+            <footer id="bottom">
+                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
 
-	                <ul role="navigation">
-	                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
-	                    <?php if(twitter_account()): ?>
-	                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
-	                    <?php endif; ?>
+                <ul role="navigation">
+                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
+                    <?php if(twitter_account()): ?>
+                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
+                    <?php endif; ?>
 
-	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
+                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
 
-	                    <li><a href="/" title="Return to my website.">Home</a></li>
-	                </ul>
-	            </footer>
-	            
-	        </div>
+                    <li><a href="/" title="Return to my website.">Home</a></li>
+                </ul>
+            </footer>
+            
         </div>
+        <script src="<?php echo theme_url('/js/jquery.min.js'); ?>"></script>
         <script src="<?php echo theme_url('/js/bootstrap.min.js'); ?>"></script>
     </body>
 </html>
--- header.php
+++ header.php
@@ -36,28 +36,44 @@
 		<?php endif; ?>
 	</head>
 	<body class="<?php echo body_class(); ?>">
-		<div class="main-wrap">
-			<div class="slidey" id="tray">
-				<div class="wrap">
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bootstrap theme</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 					<form id="search" action="<?php echo search_url(); ?>" method="post">
 						<label for="term">Search my blog:</label>
 						<input type="search" id="term" name="term" placeholder="To search, type and hit enter&hellip;" value="<?php echo search_term(); ?>">
 					</form>
-
-					<aside>
-						<b>Categories</b>
-						<ul>
-						<?php while(categories()): ?>
-							<li>
-								<a href="<?php echo category_url(); ?>" title="<?php echo category_description(); ?>">
-									<?php echo category_title(); ?> <span><?php echo category_count(); ?></span>
-								</a>
-							</li>
-						<?php endwhile; ?>
-						</ul>
-					</aside>
-				</div>
-			</div>
 
 			<header id="top">
 				<a id="logo" href="<?php echo base_url(); ?>"><?php echo site_name(); ?></a>
